a83c0cf52d29f16f9e05002b22a566e06ca0f2bd
Simon Johnston <simonjohnston@Simons-MacBook-Air.local>
2021-02-22 18:55:08 -0700

    implimented chat engine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+	return (
+		<ChatEngine
+			height='100vh'
+			userName='Sjohnston92'
+			userSecret='Backcountry1!'
+			projectID='51b8dd27-b7ab-4d38-bbc1-6f67f3edd516'
+		/>
+	);
 }
 
-export default App;
+export default App
